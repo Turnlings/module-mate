50fceecc3d802aa2ae59919b6ba19c5bdcd67ccb
Ryan Turner <rturner7@sheffield.ac.uk>
2025-05-28 17:23:35 +0100

    Added base devise files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :uni_modules do
     resources :exams
     resources :timelogs
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_24_180235) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_28_162155) do
   create_table "exams", force: :cascade do |t|
     t.decimal "weight"
     t.string "name"
@@ -45,6 +45,18 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_24_180235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "credits"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "exams", "uni_modules"
