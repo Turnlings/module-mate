1e46ca261138e66e18133c96f167e5816c2bde56
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2025-06-28 12:50:29 +0100

    feat: add stats to semester page

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -4,10 +4,20 @@ class Semester < ApplicationRecord
   has_many :uni_modules, dependent: :destroy
   belongs_to :year
 
+  def credits 
+    uni_modules.sum(:credits)
+  end
+
   def weighted_average(user)
     valid_modules = uni_modules.reject { |m| m.weighted_average(user).nil? }
     total_weight = valid_modules.sum(&:credits)
     weighted_sum = valid_modules.sum { |m| m.credits * m.weighted_average(user) }
+    total_weight.zero? ? 0 : (weighted_sum / total_weight)
+  end
+
+  def achieved_score(user)
+    total_weight = uni_modules.sum(&:credits)
+    weighted_sum = uni_modules.sum { |m| m.credits * m.achieved_score(user) }
     total_weight.zero? ? 0 : (weighted_sum / total_weight)
   end
 end
