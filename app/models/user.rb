6a7ebc64cebf5bb84e03612f70f1fcf55e5e26ab
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2025-07-16 23:04:05 +0100

    feat: add dashboard stats to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,20 @@ class User < ApplicationRecord
   has_many :exams, through: :exam_results
   has_many :uni_modules, through: :uni_module_targets
   has_many :timelogs, dependent: :destroy
+
+  def credits
+    years.sum{|year| year.credits}
+  end
+
+  def weighted_average
+    return 0 if years.empty?
+    total_weight = years.sum { |year| year.semesters.sum(&:credits) }
+    weighted_sum = years.sum { |year| year.semesters.sum { |semester| semester.credits * semester.weighted_average(self) } }
+    total_weight.zero? ? 0 : (weighted_sum / total_weight)
+  end
+
+  def achieved_score
+    return 0 if years.empty?
+    years.sum { |year| year.achieved_score(self) } / years.size
+  end
 end
