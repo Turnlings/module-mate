bc1f806ac370a0a8cf28fd03d9786f7e8193eb8e
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2025-07-17 13:46:21 +0100

    feat: add year progress to dashboard

--- app/models/year.rb
+++ app/models/year.rb
@@ -8,6 +8,7 @@ class Year < ApplicationRecord
     semesters.includes(:uni_modules).sum { |s| s.credits }
   end
 
+  # The average of all the grades of the semesters in this year
   def weighted_average(user)
     return 0 if semesters.empty?
     total_weight = semesters.sum { |s| s.credits }
@@ -15,6 +16,13 @@ class Year < ApplicationRecord
     total_weight.zero? ? 0 : (weighted_sum / total_weight)
   end
 
+  # The percentage of credits completed by the user in this year
+  def progress(user)
+    completed_credits = semesters.sum { |s| s.progress(user) }
+    completed_credits / semesters.count
+  end
+
+  # The accumulated score of all the completed exams in this year
   def achieved_score(user)
     return 0 if semesters.empty?
     semesters.sum { |semester| semester.achieved_score(user) } / semesters.size
