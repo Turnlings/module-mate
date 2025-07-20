e637e5032240e75fc555d890cdbb58da79a3cca0
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2025-07-20 14:00:36 +0100

    refactor: load upcoming assessments in the same way as pinned modules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,15 @@ class ApplicationController < ActionController::Base
     return unless current_user
 
     @pinned_modules = current_user.pinned_modules.order(:code)
+
+    @uni_modules = UniModule.joins(semester: :year)
+                            .where(years: { user_id: current_user.id })
+                            .includes(:timelogs, semester: :year)
+                            
+    @next_exams = Exam.joins(:uni_module)
+                      .where('due > ?', Time.current)
+                      .where(uni_modules: { id: @uni_modules.map(&:id) })
+                      .order(:due)
+                      .limit(3)
   end
 end
