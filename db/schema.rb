e10485a6310481e92ba85082f94fadd91cf486d1
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2025-08-19 22:49:04 +0100

    chore: add missing migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_10_203319) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_19_214716) do
   create_table "exam_results", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "exam_id", null: false
