2042038c80b5a81a034e031ce841611163114b5b
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2025-08-29 17:00:47 +0100

    test: add test to check bubbling up is working

---
+++ spec/system/partial_cache_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Partial caching', type: :system do
+  let(:user) { create(:user) }
+
+  context 'when you update an exam result' do
+    it 'bubbles up to updating the year cache' do
+      login_as user
+
+      exam = create(:exam, user: user)
+
+      # Check year is set up and cache it
+      visit root_path
+      expect(page).not_to have_content '50%'
+      expect(page).to have_content '0%'
+
+      # Set a result
+      visit uni_module_path(exam.uni_module)
+      find("a[title='Edit #{exam.name} Score']").click
+
+      fill_in 'exam_result_score', with: 50
+      click_on 'Save'
+
+      # Chek the year cards have updated
+      visit root_path
+      expect(page).to have_content '50%'
+    end
+  end
+end
