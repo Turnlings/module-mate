72aeee77575a51d6b960f54611aa4fac2ae26032
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2026-01-05 22:51:54 +0000

    style: move account to bottom

--- app/javascript/packs/theme.js
+++ app/javascript/packs/theme.js
@@ -1,31 +1,33 @@
 document.addEventListener("turbo:load", () => {
   const doc = document.documentElement;
-  const toggleBtn = document.getElementById("theme-toggle");
+  const toggleBtns = document.querySelectorAll(".theme-toggle");
 
   // Restore saved theme
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme) doc.dataset.theme = savedTheme;
 
-  if (!toggleBtn) { return; }
+  if (!toggleBtns.length) { return; }
 
-  // Add event listener
-  toggleBtn.addEventListener("click", () => {
-    const newTheme = doc.dataset.theme === "light" ? "dark" : "light";
-    doc.dataset.theme = newTheme;
-    localStorage.setItem("theme", newTheme);
+  // Add event listener to all toggle buttons
+  toggleBtns.forEach(toggleBtn => {
+    toggleBtn.addEventListener("click", () => {
+      const newTheme = doc.dataset.theme === "light" ? "dark" : "light";
+      doc.dataset.theme = newTheme;
+      localStorage.setItem("theme", newTheme);
 
-    // Change just the theme colours of chartkick charts
-    Object.values(Chartkick.charts).forEach(chart => {
-      const currentColors = chart.options.colors || [];
-      const newColors = chartColors(); // 5 theme-specific colors
+      // Change just the theme colours of chartkick charts
+      Object.values(Chartkick.charts).forEach(chart => {
+        const currentColors = chart.options.colors || [];
+        const newColors = chartColors(); // 5 theme-specific colors
 
-      // Overwrite only the first 5, keep the rest
-      const mergedColors = [...newColors, ...currentColors.slice(newColors.length)];
+        // Overwrite only the first 5, keep the rest
+        const mergedColors = [...newColors, ...currentColors.slice(newColors.length)];
 
-      chart.updateData(
-        chart.getData(),
-        Object.assign({}, chart.options, { colors: mergedColors })
-      );
+        chart.updateData(
+          chart.getData(),
+          Object.assign({}, chart.options, { colors: mergedColors })
+        );
+      });
     });
   });
 });
