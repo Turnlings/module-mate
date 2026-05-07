543e5ba6e373484965da9667b6173b360c321863
Ryan Turner <100864486+Turnlings@users.noreply.github.com>
2026-05-07 16:41:10 +0100

    fix: set timezone to London rather than UTC (#138)

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -12,7 +12,7 @@ class ExamsController < ApplicationController
 
   # GET /exams/1 or /exams/1.json
   def show
-    time_zone = TZInfo::Timezone.get(Rails.application.config.time_zone)
+    time_zone = Time.find_zone(Rails.application.config.time_zone)
     @time_until_due = @exam.time_until_due(time_zone.now)
     @uni_module = @exam.uni_module
     @semester = @uni_module.semesters.first
